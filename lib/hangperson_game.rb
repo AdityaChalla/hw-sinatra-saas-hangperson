7a4453e044e95657cc8d4076d11ff12c97944e12
adityachalla <aditya_cj@berkeley.edu>
2016-02-07 07:37:32 +0000

    Finished Part 0, 1, 2

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -8,8 +8,68 @@ class HangpersonGame
   # def initialize()
   # end
   
+  attr_accessor :word
+  attr_accessor :guesses
+  attr_accessor :wrong_guesses
+  
+
+  
   def initialize(word)
     @word = word
+    @guesses = ""
+    @wrong_guesses = ""
+  end
+  
+  def guess(letter)
+    #debugger
+    if !!(letter =~ /[A-Za-z]/)
+   
+      letter = letter.downcase
+    else
+      raise ArgumentError.new('bad letter')
+    end
+    if @word.include? letter
+      if !(@guesses.include? letter)
+        @guesses += letter
+        return true
+      else
+        return false
+      end
+    
+    else
+      if !(@wrong_guesses.include? letter)
+        @wrong_guesses += letter
+        return true
+      else
+        return false
+      end
+    end
+  end
+  
+  def word_with_guesses
+    rtn_str = ''
+    word_array = word.chars.to_a
+    word_array.each do |x|
+
+      if @guesses.include? x
+        rtn_str+=x
+      else
+        rtn_str+='-'
+      end
+    end
+    return rtn_str
+  end
+  
+  def check_win_or_lose
+    word = @word
+    new_word = self.word_with_guesses
+    if @wrong_guesses.length == 7
+      return :lose
+    elsif new_word == word
+      return :win
+    elsif new_word.include? '-'
+      return :play
+    end
   end
 
   def self.get_random_word
@@ -18,5 +78,7 @@ class HangpersonGame
     uri = URI('http://watchout4snakes.com/wo4snakes/Random/RandomWord')
     Net::HTTP.post_form(uri ,{}).body
   end
+  
+
 
 end
